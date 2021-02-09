033b512f22859289c88305a87827449fd7c92599
Dylan Mullins <dylanemullins92@gmail.com>
2021-02-09 12:23:16 -0600

    Added create.html, updated index.html

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -2,6 +2,7 @@ package org.launchcode.codingevents.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
@@ -9,12 +10,19 @@ import java.util.Arrays;
 import java.util.List;
 
 @Controller
+@RequestMapping(value="events")
 public class EventController {
-	@RequestMapping(value="events")
+
+	@GetMapping
 	public String displayAllEvents(Model model) {
 		List<String> events = new ArrayList<>(Arrays.asList("Polka Concert", "Pancake Baking with Bertie", "Spinning " +
 				"Tops"));
 		model.addAttribute("events", events);
 		return "events/index";
+	}
+
+	@GetMapping("create")
+	public String renderCreateEventForm() {
+		return "events/create";
 	}
 }
