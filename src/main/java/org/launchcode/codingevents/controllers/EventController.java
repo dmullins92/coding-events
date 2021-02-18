7d9c7d5a2052ec7a31691dadecb7ae8492ff4334
Dylan Mullins <dylanemullins92@gmail.com>
2021-02-18 11:54:16 -0600

    created new display-errors branch

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -4,8 +4,10 @@ import org.launchcode.codingevents.data.EventData;
 import org.launchcode.codingevents.models.Event;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,16 +18,26 @@ public class EventController {
 	@GetMapping
 	public String displayAllEvents(Model model) {
 		model.addAttribute("events", EventData.getAll());
+		model.addAttribute("title", "Coding Events");
 		return "events/index";
 	}
 
 	@GetMapping("create")
-	public String renderCreateEventForm() {
+	public String renderCreateEventForm(Model model) {
+		model.addAttribute("title", "Create Events");
+		model.addAttribute("event", new Event());
 		return "events/create";
 	}
 
 	@PostMapping("create")
-	public String processCreateEventForm(@ModelAttribute Event newEvent) {
+	public String processCreateEventForm(@ModelAttribute @Valid Event newEvent,
+	                                     Errors errors,
+	                                     Model model) {
+		if(errors.hasErrors()) {
+			model.addAttribute("title", "Create Events");
+			model.addAttribute("events", EventData.getAll());
+			return "events/create";
+		}
 		EventData.add(newEvent);
 		return "redirect:";
 	}
--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -1,20 +1,40 @@
 package org.launchcode.codingevents.models;
 
 import java.util.Objects;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 public class Event {
 
 	private final int id;
 	private static int nextId = 1;
+
+	@NotBlank
+	@Size(min = 3, max = 50, message = "Name must be between 5 and 50 characters.")
 	private String name;
+
+	@Size(max = 500, message = "Description is too long.")
 	private String description;
 
-	public Event(String name, String description) {
-		this.name = name;
-		this.description = description;
+	@NotBlank
+	@Email(message = "Must be a valid email.")
+	private String contactEmail;
+
+	public Event() {
 		this.id = nextId;
 		nextId++;
 	}
+
+	public Event(String name, String description, String contactEmail) {
+		this();
+		this.name = name;
+		this.description = description;
+		this.contactEmail = contactEmail;
+
+	}
+
+
 
 	public String getName() {
 		return name;
@@ -30,6 +50,14 @@ public class Event {
 
 	public void setDescription(String description) {
 		this.description = description;
+	}
+
+	public String getContactEmail() {
+		return contactEmail;
+	}
+
+	public void setContactEmail(String contactEmail) {
+		this.contactEmail = contactEmail;
 	}
 
 	public int getId() {
